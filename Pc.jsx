bdc93823bd088d39f6faa9d321d8d822df982f04
TarunCodesFr <akurax4@gmail.com>
2025-04-11 14:01:18 +0530

    Updates

--- Pc.jsx
+++ Pc.jsx
@@ -11,7 +11,7 @@ import React from 'react'
 import { useGLTF } from '@react-three/drei'
 
 export default function Model(props) {
-  const { nodes, materials } = useGLTF('/pc.gltf')
+  const { nodes, materials } = useGLTF('./pc.gltf')
   return (
     <group {...props} dispose={null}>
       <group scale={0.1}>
